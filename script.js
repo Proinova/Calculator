c1636edc2ebd988c3a7e80229cf758cabc472888
Eli <elisaveta.proinova@gmail.com>
2023-02-07 15:22:40 +0200

    07.02

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log("HI!");
+let a = document.getElementById("inputA").value;
+let b = document.getElementById("inputB").value;
+let result = document.getElementById("result");
+a = Number(a);
+b = Number(b);
+let c = a + b;
+console.log(result);
+result.innerText = c;

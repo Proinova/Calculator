057d07c73f4dcf77918f2f2ea385d45d74745de4
Eli <elisaveta.proinova@gmail.com>
2023-02-13 12:30:48 +0200

    13.02

---
+++ Game\/script.js
@@ -0,0 +1,6 @@
+let canvas = document.getElementById("gameCanvas");
+let context = canvas.getContext("2d");
+canvas.width = 300;
+canvas.height = 200;
+context.strokeRect(30, 40, 50, 60);
+console.log(canvas);
--- script.js
+++ script.js
@@ -1,8 +1,39 @@
-let a = document.getElementById("inputA").value;
-let b = document.getElementById("inputB").value;
+let inputA = document.getElementById("inputA");
+let inputB = document.getElementById("inputB");
+let sumButton = document.getElementById("sumButton");
+let razButton = document.getElementById("razButton");
+let delButton = document.getElementById("delButton");
+let umnButton = document.getElementById("umnButton");
 let result = document.getElementById("result");
-a = Number(a);
-b = Number(b);
-let c = a + b;
-console.log(result);
-result.innerText = c;
+sumButton.onclick = function() {
+
+	a = Number(inputA.value);
+	b = Number(inputB.value);
+	let c = a + b;
+	result.innerText = c;
+
+}
+razButton.onclick = function() {
+
+	a = Number(inputA.value);
+	b = Number(inputB.value);
+	let c = a - b;
+	result.innerText = c;
+
+}
+delButton.onclick = function() {
+
+	a = Number(inputA.value);
+	b = Number(inputB.value);
+	let c = a / b;
+	result.innerText = c;
+
+}
+umnButton.onclick = function() {
+
+	a = Number(inputA.value);
+	b = Number(inputB.value);
+	let c = a * b;
+	result.innerText = c;
+
+}

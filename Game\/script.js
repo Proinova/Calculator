af56162ce555973e2fd72192900cdba47def89ee
Eli <elisaveta.proinova@gmail.com>
2023-02-17 11:31:16 +0200

    geroi+pole

--- Game\/script.js
+++ Game\/script.js
@@ -1,18 +1,32 @@
 let canvas = document.getElementById("gameCanvas");
 let context = canvas.getContext("2d");
-let nx = 4;
-let ny = 3;
-let sqside = 50;
+let nx = 8;
+let ny = 8;
+let sqside = 60;
 let herox = 2;
 let heroy = 1;
+let heroimg = new Image();
+heroimg.src = "thinker_bell.jpg";
 canvas.width = nx * sqside;
 canvas.height = ny * sqside;
-for (let i=0; i<nx; i++) {
+
+function drawMap() {
+	context.clearRect(0, 0, canvas.width, canvas.height);
+	context.drawImage(heroimg, herox * sqside, heroy * sqside, 		sqside, sqside);
+	for (let i=0; i<nx; i++) {
 		for (let j=0; j<ny; j++) {
 			context.strokeRect(i*sqside, j*sqside, sqside, sqside);
 		}
+	}
 }
-//context.fillRect(herox * sqside, heroy * sqside, sqside, sqside);
-let heroimg = new Image();
-heroimg.src = "thinker_bell.jpg";
-context.drawImage(heroimg, herox * sqside, heroy * sqside, sqside, sqside);
+
+drawMap();
+
+canvas.onclick = function(d) {
+		let x = d.x - canvas.offsetLeft;
+		let y = d.y - canvas.offsetTop;
+		herox = Math.floor(x / sqside);
+		heroy = Math.floor(y / sqside);
+		console.log(herox + " " + heroy);
+		drawMap();
+}
